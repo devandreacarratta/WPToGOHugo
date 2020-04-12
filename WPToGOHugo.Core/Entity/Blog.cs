427a675e8010195fea18263ddbad4b353d07b7fc
Andrea Carratta <46504271+devandreacarratta@users.noreply.github.com>
2020-04-12 09:45:02 +0200

    Create Entity

---
+++ WPToGOHugo.Core/Entity/Blog.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace WPToGOHugo.Core.Entity
+{
+    public class Blog
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("content")]
+        public string Content { get; set; }
+
+        [JsonProperty("link")]
+        public Uri Link { get; set; }
+
+        [JsonProperty("category")]
+        public List<Category> Category { get; set; }
+
+        [JsonProperty("tags")]
+        public List<Tag> Tags { get; set; }
+
+        [JsonProperty("pubDate")]
+        public string PubDate { get; set; }
+
+        [JsonProperty("dcCreator")]
+        public string DcCreator { get; set; }
+
+        [JsonProperty("seo_metadesc")]
+        public string SeoMetadesc { get; set; }
+    }
+}
---
+++ WPToGOHugo.Core/Entity/Category.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+
+namespace WPToGOHugo.Core.Entity
+{
+    public  class Category
+    {
+        [JsonProperty("term_id")]
+        public long TermId { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("slug")]
+        public string Slug { get; set; }
+
+        [JsonProperty("term_group")]
+        public long TermGroup { get; set; }
+
+        [JsonProperty("term_taxonomy_id")]
+        public long TermTaxonomyId { get; set; }
+
+        [JsonProperty("taxonomy")]
+        public string Taxonomy { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("parent")]
+        public long Parent { get; set; }
+
+        [JsonProperty("count")]
+        public long Count { get; set; }
+
+        [JsonProperty("filter")]
+        public string Filter { get; set; }
+
+        [JsonProperty("cat_ID")]
+        public long CatId { get; set; }
+
+        [JsonProperty("category_count")]
+        public long CategoryCount { get; set; }
+
+        [JsonProperty("category_description")]
+        public string CategoryDescription { get; set; }
+
+        [JsonProperty("cat_name")]
+        public string CatName { get; set; }
+
+        [JsonProperty("category_nicename")]
+        public string CategoryNicename { get; set; }
+
+        [JsonProperty("category_parent")]
+        public long CategoryParent { get; set; }
+    }
+}
