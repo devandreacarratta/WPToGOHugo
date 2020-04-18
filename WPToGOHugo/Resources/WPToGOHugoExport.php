30534f8b662875b56d96b1a38207df908f763611
Andrea Carratta <andrea.carratta.dev@outlook.it>
2020-04-18 11:04:44 +0200

    Update PHP Page

--- WPToGOHugo/Resources/WPToGOHugoExport.php
+++ WPToGOHugo/Resources/WPToGOHugoExport.php
@@ -1,19 +1,12 @@
 <?php
 
-$postCount = 10; 
-
-if($_GET["postCount"] != "")
-{ 
-    $postCount =$_GET["postCount"];  
-}
-
 if ( !defined('ABSPATH') ) {
 	/** Set up WordPress environment */
 	require_once( dirname( __FILE__ ) . '/wp-load.php' );
 }
 
 
-function CleanContent( $content ) {
+function remove_shortcodes( $content ) {
     return ($content) ;
 }
 
@@ -24,25 +17,24 @@ $idxItem = 0;
 
 while ( have_posts() ) : the_post();
 
-    $response[$idxItem] = array(
-        'id' => get_the_ID(),
-        'slug' => get_post_field( 'post_name', get_post() ),
-        'title' => get_the_title(),
-        'content' => CleanContent( get_the_content() ),
-        'link'=> get_the_permalink(),
-        'category' => get_the_category(),
-        'tags' => get_the_tags(),
-        'pubDate' => mysql2date('D, d M Y H:i:s +0000', get_post_time('Y-m-d H:i:s', true), false),
-        'dcCreator' => get_the_author(),
-        'seo_metadesc' => get_post_meta(get_the_ID(), '_yoast_wpseo_metadesc', true),
-        'seo_focuskw' => get_post_meta(get_the_ID(), '_yoast_wpseo_focuskw', true),
-        'seo_image' => get_the_post_thumbnail_url(),
-    );
+	$response[$idxItem] = array(
+		'id' => get_the_ID(),
+		'slug' => get_post_field( 'post_name', get_post() ),
+		'title' => get_the_title(),
+		'content' => remove_shortcodes( get_the_content() ),
+		'link'=> get_the_permalink(),
+		'category' => get_the_category(),
+		'tags' => get_the_tags(),
+		'pubDate' => mysql2date('D, d M Y H:i:s +0000', get_post_time('Y-m-d H:i:s', true), false),
+		'dcCreator' => get_the_author(),
+		'seo_metadesc' => get_post_meta(get_the_ID(), '_yoast_wpseo_metadesc', true),
+		'seo_focuskw' => get_post_meta(get_the_ID(), '_yoast_wpseo_focuskw', true),
+		'seo_image' => get_the_post_thumbnail_url(),
+	);
 
-$idxItem +=1;
+	$idxItem +=1;
 
 endwhile;
-
 
 header("Content-Type: application/json");
 echo json_encode($response); 
