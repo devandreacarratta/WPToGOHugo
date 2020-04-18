8ebf337b5c6d070dbbb432e23fea6b3f803077d4
Andrea Carratta <andrea.carratta.dev@outlook.it>
2020-04-18 10:29:51 +0200

    CleanerDiviCodeSnippetBase64

--- WPToGOHugo/Cleaner/CleanerDiviCodeSnippetBase64.cs
+++ WPToGOHugo/Cleaner/CleanerDiviCodeSnippetBase64.cs
@@ -10,13 +10,18 @@ namespace WPToGOHugo.Cleaner
         private const string FORMAT_SECTION = "```";
 
         private const string TAG_CODE = "et_pb_dmb_code_snippet";
-      
-        public string Run(string value)
+
+        public override string Run(string value)
         {
-          
+            if (value.IndexOf(TAG_CODE) == -1)
+            {
+                return value;
+            }
+
+            value = base.Run(value);
             while (value.IndexOf(TAG_CODE) != -1)
             {
-                value = base.Run(value);
+
                 value = this.RemoveTagWithBase64(value, $"[{TAG_CODE}", $"[/{TAG_CODE}]", FORMAT_SECTION);
             }
 
