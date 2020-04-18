9ee22f575da84cb2eb26c5b11ed0af21e1de54da
Andrea Carratta <andrea.carratta.dev@outlook.it>
2020-04-18 10:57:39 +0200

    CleanerDiviCodeSnippetBase64

--- WPToGOHugo/Cleaner/CleanerDiviCodeSnippetBase64.cs
+++ WPToGOHugo/Cleaner/CleanerDiviCodeSnippetBase64.cs
@@ -1,35 +1,20 @@
-﻿using System;
-using System.Text;
+﻿using System.Text;
 using System.Text.RegularExpressions;
 
 namespace WPToGOHugo.Cleaner
 {
     internal class CleanerDiviCodeSnippetBase64 : BaseCleanerDivi
     {
-
         private const string FORMAT_SECTION = "```";
 
         private const string TAG_CODE = "et_pb_dmb_code_snippet";
-        private const string TAG_TEXT = "et_pb_dmb_code_snippet";
-
-        //string pattern = @"(\[et_pb_text\s((.*?))\](?<pretextcontent>(.*?))\[\/et_pb_text\])|(\[et_pb_dmb_code_snippet\s(.*?)\](?<code64>.*?)\[.*?\])";
-
 
         public override string Run(string value)
         {
-
             if (value.IndexOf(TAG_CODE) == -1)
             {
                 return value;
             }
-
-            value = base.Run(value);
-
-            value = value.Trim();
-
-
-            StringBuilder sb = new StringBuilder();
-
 
             string pattern = @$"(?>^|\n|\[[^\]\n]*\]\n*)(?'content'[^\[]+)";
 
@@ -40,33 +25,31 @@ namespace WPToGOHugo.Cleaner
                 return value;
             }
 
+            StringBuilder sb = new StringBuilder();
+
             foreach (Match match in items)
             {
                 string content = match.Groups["content"].Value;
                 if (string.IsNullOrEmpty(content) == false)
                 {
-
-                    try{
-                        var decodeText=TextHelper.Base64Decode(content);
+                    try
+                    {
+                        var decodeText = TextHelper.Base64Decode(content);
 
                         sb.AppendLine(FORMAT_SECTION);
                         sb.AppendLine(decodeText);
                         sb.AppendLine(FORMAT_SECTION);
-
-
                     }
-                    catch{
+                    catch
+                    {
                         sb.AppendLine(content);
                     }
-
                 }
-
-               
             }
 
-            value = sb.ToString();
+            string result = sb.ToString();
 
-            return value;
+            return result;
         }
     }
 }
